24f119d4e8bb46cafb9ac6da254da2744a96381f
Steve Loranz <sloranz@redhat.com>
2013-10-21 17:45:36 -0500

    starting to cleanup/update

--- tests/testcache.py
+++ tests/testcache.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python
+# coding=utf-8
+
+#   Copyright 2013 Red Hat, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 import sys
-sys.path.append("../")
+sys.path.append("../novaimagebuilder")
 from MockStackEnvironment import MockStackEnvironment as StackEnvironment
-from CacheManager import CacheManager
+from novaimagebuilder.CacheManager import CacheManager
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s thread(%(threadName)s) Message: %(message)s')
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(name)s thread(%(threadName)s) Message: %(message)s')
 
 class MockOSPlugin(object):
     
